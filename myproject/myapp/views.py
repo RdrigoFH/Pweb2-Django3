4bc334f4f973c20db4541d0ec9250881998d50b2
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-15 19:25:38 -0500

    Se ingresaron datos en la vista, estos son los que posteriormente nos serviran para representar el uso de forloop

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 def index(request):
     context = {
-      
+        'numbers': [1, 2, 3, 4, 5],
+        'words': ['apple', 'banana', 'cherry'],
+        'users': [
+            {'name': 'Alan', 'age': 25},
+            {'name': 'Marcelo', 'age': 30},
+            {'name': 'Alberto', 'age': 35}
+        ]
     }
     return render(request, 'index.html', context)
