0798d4249afd66e0a1c92b2b9633400895e6158c
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-15 19:23:24 -0500

    Se crea una vista para renderizar una plantilla, aun no se insertaron los datos, es por el momento una estructura en blanco

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    context = {
+      
+    }
+    return render(request, 'index.html', context)
