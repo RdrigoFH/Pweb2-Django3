da7577f28c064081101930a1f957b21701bc57c8
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-15 22:49:41 -0500

    Se arreglo la configuracion de urls en my app/urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import index
 from . import views
 
 urlpatterns = [
     path('crear_persona/', views.crear_persona, name='crear_persona'),
-    path('catalog/', index, name='index'),  
 ]
