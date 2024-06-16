93b118b3862dedfa7081162f14d1ff9be404c3f1
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-15 22:48:01 -0500

    Se agrega la url para la vista crear persona

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index
+from . import views
 
 urlpatterns = [
+    path('crear_persona/', views.crear_persona, name='crear_persona'),
     path('catalog/', index, name='index'),  
 ]
